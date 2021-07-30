d88867c04d1f1602c3ff49a4ec014db0e6686880
kutay-ugurlu <kutay.ugurlu.1@gmail.com>
2021-07-30 16:14:48 +0300

    Final Version

---
+++ back_projection.m
@@ -0,0 +1,78 @@
+%% PART 2 : RECONSTRUCTION
+% Inputs : Size of the image, Projection matrix.
+% Outputs : Image
+
+function [Image] = back_projection(RowNumber_I, ColumnNumber_I, PROJECTIONS)
+
+[angles, t] = size(PROJECTIONS);
+PROJECTIONS = transpose(PROJECTIONS);
+angle_step_size = 180/angles;
+Image = zeros(RowNumber_I, ColumnNumber_I);
+left_end = -0.5*RowNumber_I;
+right_end = -1 * left_end;
+X_grid = left_end : right_end;
+Y_grid = X_grid;
+thetas = deg2rad(0:angle_step_size:180-angle_step_size) ; 
+beams = linspace(left_end*sqrt(2),right_end*sqrt(2),t);
+for angle = 1:length(thetas)
+    theta = thetas(angle);
+    for ray = 1:length(beams)
+        t = beams(ray);   
+        %%
+        % Creating intersection vectors for each angle using the equation
+        % _t_ = cos($$ \  \theta $$)  _x_  +  sin($$ \  \theta $$)  _y_
+
+        intersect_y = ((t - X_grid * cos(theta)) / sin(theta));
+        intersect_x = ((t - Y_grid * sin(theta)) / cos(theta));
+        %%
+        % Putting the sorted intersections in a matrix where first column is x,
+        % second is y and merge them: 
+        INTERSECTS_x = [X_grid'  intersect_y'];
+        INTERSECTS_y = [intersect_x'  Y_grid'];
+        INTERSECTS_all = [INTERSECTS_x ; INTERSECTS_y];
+        % INTERSECTS_all = sortrows(unique(INTERSECTS_all,'rows'));
+        INTERSECTS_all = sortrows(uniquetol(INTERSECTS_all,'ByRows',1e-10));
+        % The above line is for Lena
+
+        %%
+        % Discarding the intersections out of the grid by conditionally
+        % selecting the rows
+        INTERSECTS_all = INTERSECTS_all(INTERSECTS_all(:,1)>=left_end & INTERSECTS_all(:,1)<=right_end & INTERSECTS_all(:,2)>=left_end & INTERSECTS_all(:,2)<=right_end,:);
+        %%
+        % Assigning 0 projection value for 1 point intersections.
+        if size(INTERSECTS_all,1) == 1
+        continue
+        end
+        %%
+        % Using Pisagor to compute distances travelled in pixels by
+        % computing the $$ l_2 $$ norm row vectors of intersection matrix
+        weights = vecnorm(diff(INTERSECTS_all),2,2); 
+        %%
+        % Arithmetic mean of consecutive elements to find mid point: To
+        % find the mid point between consecutive intersections, 2 point
+        % moving average is used with factor 0.5.
+        midpoints_x = movsum(INTERSECTS_all(:,1),2) * 0.5;
+        midpoints_y = movsum(INTERSECTS_all(:,2),2) * 0.5;
+        
+        %%
+        % MATLAB function movesum padds a 0 to the beginning of the
+        % vector. To get rid of this extra entry at the beginning, vectors
+        % are sliced.
+        % This block of code is problematic when there is 1 intersection. 
+        % Hence, if block above is added.
+        midpoints_y = midpoints_y(2:end); 
+        midpoints_x = midpoints_x(2:end);
+        
+        %%
+        % The pixels that beam passes through are found.
+        row_pixel_indices = right_end - floor(midpoints_y);
+        column_pixel_indices = right_end + ceil(midpoints_x);
+      
+        %%
+        % Projections are weighted with the distances that the beam travels inside a pixel and the product is written on the relevant pixel.               
+        for i = 1:length(column_pixel_indices)
+            Image(row_pixel_indices(i),column_pixel_indices(i)) = Image(row_pixel_indices(i),column_pixel_indices(i)) + weights(i)*PROJECTIONS(ray, angle);
+        end 
+    end
+end
+end
---
+++ filtered_back_projection_bart.m
@@ -0,0 +1,39 @@
+%% FILTERED BACK PROJECTION 
+
+function [Image] = filtered_back_projection(RowNumber_I, ColumnNumber_I, PROJECTIONS)
+[angles, L_c] = size(PROJECTIONS); % L_c : length of columns
+PROJECTIONS = transpose(PROJECTIONS);
+%% First filter the projections, then call back_projection.m script.
+% For now, filter is set to ramp. Then, filter is designed in frequency
+% domain and inverse FFT of it is taken to convolve the filter and
+% projections in space domain. 
+
+ramp = [0 1:50 50:-1:1]; % Ram-Lak
+
+%% Different Filters
+% To apply the selected filter, the user can just uncomment lines
+% regarding that filter.
+
+% w = window(@hamming, 100);
+% ramp = ramp .* [0 fftshift(w')];    
+
+w = window(@barthannwin, 100);
+ramp = ramp .* [0 fftshift(w')];  
+
+%% Filtering the projections in a loop through the columns of Projections Matrix
+% The filter is convolved with the projections for a fixed angle.
+filter_f = fftshift(transpose(ifft(ramp)));
+for i = 1:angles
+    column = PROJECTIONS(:,i);
+    filtered_column = conv(filter_f, column);
+    if mod(length(filtered_column),2) == 1 
+        filtered_column = [filtered_column; 0];
+    end
+    first_idx = ceil(0.5*(length(filtered_column)-L_c));
+    filtered_column = filtered_column(first_idx:first_idx+L_c-1);
+    PROJECTIONS(:,i) = filtered_column;
+end
+PROJECTIONS = transpose(PROJECTIONS);
+Image = back_projection(RowNumber_I, ColumnNumber_I, PROJECTIONS);
+end
+

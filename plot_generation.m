ea397baff96b9f74abe278bd888ce260e84a8fae
kutay-ugurlu <kutay.ugurlu.1@gmail.com>
2022-03-29 19:34:48 +0300

    Plots generated, Theory completed

--- plot_generation.m
+++ plot_generation.m
@@ -1,25 +1,33 @@
-
-ramp = [0 1:50 50:-1:1]; % Ram-Lak
-filter_f = fftshift(transpose(ifft(ramp)));
+close all; clc; clear;
+%%
+original_ramp = [0 1:50 50:-1:1]; % Ram-Lak
+filter_f = fftshift(transpose(ifft(original_ramp)));
 display(norm(imag(filter_f))/norm(real(filter_f)))
-plot(fftshift(ramp))
-
+plot(-50:50,fftshift(original_ramp))
+title('Frequency Response Characteristics of Ram-Lak Filter')
+xlabel('Frequency Bins')
+ylabel('$|\mathcal{F}(filter)|$',Interpreter='latex')
 %% 
 
 w = window(@barthannwin, 100);
-ramp = [0 fftshift(w')];  
+ramp = original_ramp .* [0 fftshift(w')];  
 filter_f = fftshift(transpose(ifft(ramp)));
 display(norm(imag(filter_f))/norm(real(filter_f)))
 figure
-plot(ramp)
-
-
+plot(ramp(2:end))
+set(gca,'XTickLabel',-50:10:50)
+title({'Frequency Response Characteristics of','Bartlett Hann window-convolved Ram-Lak Filter'})
+xlabel('Frequency Bins')
+ylabel('$|\mathcal{F}(filter)|$',Interpreter='latex')
 %%
 
 w = window(@hamming, 100);
-ramp = [0 fftshift(w')]; 
+ramp = original_ramp .* [0 fftshift(w')]; 
 filter_f = fftshift(transpose(ifft(ramp)));
 display(norm(imag(filter_f))/norm(real(filter_f)))
 figure
-plot(ramp)
-
+plot(ramp(2:end))
+set(gca,'XTickLabel',-50:10:50)
+title({'Frequency Response Characteristics of','Hamming window-convolved Ram-Lak Filter'})
+xlabel('Frequency Bins')
+ylabel('$|\mathcal{F}(filter)|$',Interpreter='latex')
